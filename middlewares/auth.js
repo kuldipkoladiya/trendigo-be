8135b38f78f92111692d4c44accff8fc97900d11
parag-radadiya <parag.180410107091@gmail.com>
2026-05-12 11:06:07 +0530

    fix auth for admin

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,34 +2,53 @@ import passport from 'passport';
 import httpStatus from 'http-status';
 import ApiError from 'utils/ApiError';
 import { TokenExpiredError } from 'jsonwebtoken';
+import mongoose from 'mongoose';
 import { roleservice } from '../services';
 
 const verifyCallback = (req, resolve, reject, roles) => async (err, user, info) => {
-  if (err || info || !user) {
-    if (info instanceof TokenExpiredError) {
-      // This state that token is Invalid and we can send status code 498 so that user can call the refresh token if we have any
-      return reject(new ApiError(httpStatus.extra.unofficial.INVALID_TOKEN, 'Token Expired'));
-    }
-    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
-  }
-  req.user = user;
+  try {
+    if (err || info || !user) {
+      if (info instanceof TokenExpiredError) {
+        return reject(new ApiError(httpStatus.extra.unofficial.INVALID_TOKEN, 'Token Expired'));
+      }
 
-  if (roles) {
-    const getRole = await roleservice.getOneRole({
-      _id: req.user.role,
-    });
-    if (typeof role === 'object') {
-      if (!roles.includes(getRole.role)) {
-        reject(new ApiError(httpStatus.UNAUTHORIZED, 'You does not have permission to access this route!'));
+      return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
+    }
+
+    req.user = user;
+
+    if (roles) {
+      // validate ObjectId before query
+      if (!mongoose.Types.ObjectId.isValid(req.user.role)) {
+        return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Invalid role id'));
       }
-    } else if (typeof roles === 'string') {
-      if (getRole.role !== roles) {
-        reject(new ApiError(httpStatus.UNAUTHORIZED, 'You does not have permission to access this route!'));
+
+      const getRole = await roleservice.getOneRole({
+        _id: req.user.role,
+      });
+
+      if (!getRole) {
+        return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Role not found'));
+      }
+
+      // FIXED TYPO HERE
+      if (Array.isArray(roles)) {
+        if (!roles.includes(getRole.role)) {
+          return reject(new ApiError(httpStatus.UNAUTHORIZED, 'You do not have permission to access this route!'));
+        }
+      } else if (typeof roles === 'string') {
+        if (getRole.role !== roles) {
+          return reject(new ApiError(httpStatus.UNAUTHORIZED, 'You do not have permission to access this route!'));
+        }
       }
     }
+
+    resolve();
+  } catch (error) {
+    reject(error);
   }
-  resolve();
 };
+
 const auth = (role) => async (req, res, next) => {
   return new Promise((resolve, reject) => {
     passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, role))(req, res, next);
@@ -37,4 +56,5 @@ const auth = (role) => async (req, res, next) => {
     .then(() => next())
     .catch((err) => next(err));
 };
+
 module.exports = auth;
