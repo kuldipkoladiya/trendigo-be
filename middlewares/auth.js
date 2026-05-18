3b3833075d310fd7214d08d74eb167df2e762864
parag-radadiya <parag.180410107091@gmail.com>
2026-05-18 10:45:44 +0530

    fix auth for roles

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,48 +1,72 @@
 import passport from 'passport';
 import httpStatus from 'http-status';
 import ApiError from 'utils/ApiError';
+import mongoose from 'mongoose';
 import { TokenExpiredError } from 'jsonwebtoken';
 import { roleservice } from '../services';
 
 const verifyCallback = (req, resolve, reject, roles) => async (err, user, info) => {
-  if (err || info || !user) {
-    if (info instanceof TokenExpiredError) {
-      // This state that token is Invalid and we can send status code 498 so that user can call the refresh token if we have any
-      return reject(new ApiError(httpStatus.extra.unofficial.INVALID_TOKEN, 'Token Expired'));
-    }
-    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
-  }
-  req.user = user;
+  try {
+    if (err || info || !user) {
+      if (info instanceof TokenExpiredError) {
+        return reject(
+          new ApiError(
+            (httpStatus.extra && httpStatus.extra.unofficial && httpStatus.extra.unofficial.INVALID_TOKEN) || 498,
+            'Token Expired'
+          )
+        );
+      }
 
-  if (roles) {
-    const getRole = await roleservice.getOneRole({
-      _id: req.user.role,
-    });
-    if (typeof role === 'object') {
-      if (!roles.includes(getRole.role)) {
-        reject(new ApiError(httpStatus.UNAUTHORIZED, 'You does not have permission to access this route!'));
+      return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
+    }
+
+    req.user = user;
+
+    if (roles) {
+      let userRole = req.user.role;
+
+      // If role is ObjectId then fetch role from DB
+      if (mongoose.Types.ObjectId.isValid(userRole)) {
+        const getRole = await roleservice.getOneRole({
+          _id: userRole,
+        });
+
+        userRole = getRole ? getRole.role : null;
       }
-    } else if (typeof roles === 'string') {
-      if (getRole.role !== roles) {
-        reject(new ApiError(httpStatus.UNAUTHORIZED, 'You does not have permission to access this route!'));
+
+      // Array roles check
+      if (Array.isArray(roles)) {
+        if (!roles.includes(userRole)) {
+          return reject(new ApiError(httpStatus.UNAUTHORIZED, 'You does not have permission to access this route!'));
+        }
+      }
+
+      // Single role check
+      else if (typeof roles === 'string') {
+        if (userRole !== roles) {
+          return reject(new ApiError(httpStatus.UNAUTHORIZED, 'You does not have permission to access this route!'));
+        }
       }
     }
+
+    resolve();
+  } catch (error) {
+    reject(error);
   }
-  resolve();
 };
 
-// todo : updates roles based on data base roles update in middleware( params for middleware fun is coming dynamically in some admin routs so admin can access route based on that )
 const auth = (roles) => async (req, res, next) => {
   return new Promise((resolve, reject) => {
-    // Extract token from Authorization header or query parameters
     const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : req.query.authToken;
+
     if (token) {
-      // Set the token in the headers for Passport to use
       req.headers.authorization = `Bearer ${token}`;
     }
+
     passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, roles))(req, res, next);
   })
     .then(() => next())
     .catch((err) => next(err));
 };
+
 module.exports = auth;
